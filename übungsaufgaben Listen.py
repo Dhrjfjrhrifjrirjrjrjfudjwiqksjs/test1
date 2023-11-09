2510aee847f86187b83c1b655c3292ce26888e19
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2023-11-09 15:36:57 +0100

    Add files via upload

---
+++ übungsaufgaben Listen.py
@@ -0,0 +1,18 @@
+obstsalat=["Apfel","Wassermelone","Mango"]
+print (obstsalat)
+obstsalat.append("Ananas")
+print (obstsalat)
+obstsalat.insert(0,"Orange")
+obstsalat.insert(0,"Kirsche")
+print (obstsalat)
+obstsalat.remove("Kirsche")
+print (obstsalat)
+obstsalat.pop(1)
+print (obstsalat)
+print (obstsalat.index("Ananas"))
+print (len(obstsalat))
+
+
+list_goals_sorted=[12,4,7,3]
+list_goals_sorted.sort()
+print (list_goals_sorted)
