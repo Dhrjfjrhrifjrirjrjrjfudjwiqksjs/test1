114763758720ba0f17c2d71dfe91f25c47873909
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2023-12-07 15:45:15 +0100

    Add files via upload

---
+++ MagischeRunen.py
@@ -0,0 +1,12 @@
+import csv
+A = 5
+B = 7
+C = 3
+D = 9
+E = 6
+Buchstaben = A, B, C, D, E
+with open("/home/scts/Downloads/magischeRunen.csv") as csv_file:
+    print("Enter Magische Runenkombination")
+    MagischeRunenkombination = input()
+    if Buchstaben
+    
