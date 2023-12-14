6a04c472afdd83143285d83829d85bc059b40111
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2023-12-14 15:38:24 +0100

    Add files via upload

--- MagischeRunen.py
+++ MagischeRunen.py
@@ -4,9 +4,25 @@ B = 7
 C = 3
 D = 9
 E = 6
-Buchstaben = A, B, C, D, E
+gesamtsumme = 0
 with open("/home/scts/Downloads/magischeRunen.csv") as csv_file:
-    print("Enter Magische Runenkombination")
-    MagischeRunenkombination = input()
-    if Buchstaben
+    data = csv.reader(csv_file)
+    for row in data:
+        for element in row:
+            for letter in element:
+                if letter == "A":
+                    gesamtsumme = gesamtsumme + 5
+                elif letter == "B":
+                    gesamtsumme = gesamtsumme + 7
+                elif letter == "C":
+                    gesamtsumme = gesamtsumme + 3
+                elif letter == "D":
+                    gesamtsumme = gesamtsumme + 9
+                elif letter == "E":
+                    gesamtsumme = gesamtsumme + 6
+                print(gesamtsumme)
+
+
+        
+    
     
