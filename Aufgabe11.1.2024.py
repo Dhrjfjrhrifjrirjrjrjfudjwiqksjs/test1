5fa02f26e032063b8865ce2697a3a7954d7907eb
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-01-11 15:43:05 +0100

    Add files via upload

---
+++ Aufgabe11.1.2024.py
@@ -0,0 +1,11 @@
+for Vielfaches_von3_5 in range(101):
+     
+    if (Vielfaches_von3_5 % 3==0 and Vielfaches_von3_5 % 5==0):
+        print("DöööööööööönnnnnnnnnnnneeeeeeeeeeeeeeeerrrrrrrKeeeeeeeeeeeeeeeeeeeeeeeebbbbbbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaabbbbbbbb")
+
+    elif (Vielfaches_von3_5 % 3==0):
+        print("Dööööööööööönnnnnnnnneeeeeeeeeeerrrrrrrrrrrrr!!!!!!!!!!!")
+    elif (Vielfaches_von3_5 % 5==0):
+        print("Keeeeeeeebbbbbbbbbbbaaaaaaaaaaabbbbbbbbbbbbb!!!!!!!!!")
+    else:
+        print(Vielfaches_von3_5)
