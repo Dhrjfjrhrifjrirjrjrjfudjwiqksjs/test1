bfbead0a16cac2638b174f5ac41f3c633e21b9c3
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-04-11 15:45:56 +0200

    Add files via upload

---
+++ Passwort_überprüfung.py
@@ -0,0 +1,15 @@
+def Passwort_überprüfung ():
+    x = 0
+    print ("Geben Sie Ihr Passwort ein.")
+    x = input ()
+    if len(x) >= 8 : 
+      x.isdigit()
+      print (x.isupper())
+    elif len(x) <= 8 :
+
+
+
+
+
+Passwort_überprüfung()
+
