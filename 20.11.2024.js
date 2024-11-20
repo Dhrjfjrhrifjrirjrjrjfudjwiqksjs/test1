b84561a8111fc78b421af1da6a15c0fe03f6c1c8
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-11-20 15:46:20 +0100

    Create 20.11.2024.js

---
+++ 20.11.2024.js
@@ -0,0 +1,29 @@
+let x = 5
+x = 20
+
+console.log(x)
+
+
+let name = "Liam"
+let age = 15
+let isStudent = "10c"
+
+console.log(name, age, isStudent)
+
+
+let a = 5
+let b = 10
+let c = 23
+
+console.log(a + b * c)
+
+console.log((5+10) * 23)
+
+
+Vorname = "Liam"
+Nachname = "Alt"
+
+console.log(Vorname, Nachname)
+
+
+typeof let d = 10 
