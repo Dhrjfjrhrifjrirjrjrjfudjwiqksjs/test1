3ca274497d7ca58efb99b278aa945fec5f4b99dc
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-11-20 15:30:10 +0100

    Update neu.js

--- neu.js
+++ neu.js
@@ -3,3 +3,6 @@ let y = "23"
 // Eine Zeilen Kommentar
 /* Bis geschlossen */
 console.log(y * x)
+
+Operatoren Mathe: +, -, *, /, %, Math.floo(), Math.pow()
+Logische Operatoren: ==, !=, <, >, >=, <=, ===, !==
