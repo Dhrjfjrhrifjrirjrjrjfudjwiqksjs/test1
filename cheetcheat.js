9cf810a3a01cf0d43a439a9fcba0decfc7746ecb
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-11-22 23:14:53 +0100

    Update cheetcheat.js

--- cheetcheat.js
+++ cheetcheat.js
@@ -3,3 +3,7 @@ let y ="23"
 //Eine Zeilen Kommentar
 /* Bis geschlossen */
 console.log(y * x)
+
+
+Operatoren Mathe: +, -, *, /, %, Math.floo(), Math.pow()
+Logische Operatoren: ==, !=, <, >, >=, <=, ===, !==
