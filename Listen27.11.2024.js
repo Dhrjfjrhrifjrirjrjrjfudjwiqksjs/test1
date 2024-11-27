fad11c4c98682f1d8c67d684deb2b09cd592c6f1
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-11-27 15:42:27 +0100

    Create Listen27.11.2024.js

---
+++ Listen27.11.2024.js
@@ -0,0 +1,20 @@
+let liste = [10, 20, 30]
+
+console.log(liste[1])
+
+
+let namen = ["Anna","Tom","Max"]
+namen.push("Karl")
+console.log(namen)
+
+
+let zahlen = [10,5,2,4]
+zahlen.shift(0)
+console.log(zahlen)
+
+
+let aussagen = [100,200,"große Zahlen","größere Zahlen"]
+aussagen.shift(0)
+aussagen.shift(1)
+aussagen.shift(2)
+console.log(aussagen)
