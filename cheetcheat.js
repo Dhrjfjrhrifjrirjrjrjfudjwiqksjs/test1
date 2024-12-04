cbb6f85c22a3a436fcd24cf696dc17694b659deb
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-12-04 19:57:31 +0100

    Update cheetcheat.js

--- cheetcheat.js
+++ cheetcheat.js
@@ -12,10 +12,10 @@ Logische Operatoren: ==, !=, <, >, >=, <=, ===, !==
   Listen
 
 let x =[2,3,"Hello"]
-console.log(x[0]) = 2
+console.log(x[0]) = 2 // so kann man einen Wert aussuchen der ausgegeben werden soll allerdings auch mit Angabe des Index 
 x.push(3) = [2,3,"Hello",3] // fügt einen wert ans ende der Liste
 x.pop() = "Hello",x = [2,3]
-x.shift() = 2,x = [3,"Hello"]
+x.shift() = 2,x = [3,"Hello"] // nimmt einen Wert aus der Liste heraus man muss jedoch den Index angeben
 x.slice(1,3) = [3,"Hello"] // mit slice kann man auch sachen ersetzen
 x.includes(2) = true
 
@@ -23,3 +23,22 @@ let n = [1,2,3,4]
 console.log(n.length)
 n.splice(3,0"Hallo")
 console.log(n)
+
+
+Objects
+
+let obj = {name:"Jan",age:27,address:{city:"MG"}}
+console.log(obj.name);/console.log(obj["name"])
+obj.name = "Karl"
+obj.address.city
+Object.keys(obj)
+Object.values(obj)
+
+
+
+
+
+
+
+
+
