4099cdc0eaa64786fd7bc370999a7d3d5e0f1870
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-12-04 15:45:59 +0100

    Create Objects.js

---
+++ Objects.js
@@ -0,0 +1,16 @@
+let movie = {title:"Cool Runnings", director:"Jon Turteltaub",year:1994 }
+console.log(movie.title)
+console.log(movie.director)
+console.log(movie.year)
+
+
+
+movie.year = movie.year + 30
+console.log(movie.year)
+movie.title = "Dune"
+console.log(movie.title)
+
+
+let library = {books:[{Title:"Solo Leveling",Author:"Chugong",Verfügbarkeitsstatus:"true"},{Title:"Kurokos Basketball",Author:"Tadatoshi Fujimaki",Verfügbarkeitsstatus:"true"},{Title:"Shangri La Frontier",Author:"Katarina",Verfügbarkeitsstatus:"False"}]}
+console.log(library.books)
+console.log(library.books.splice)
