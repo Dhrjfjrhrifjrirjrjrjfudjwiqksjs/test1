c2ce9dfdf5da8e44bc24f6630ffa8ec7e62e174d
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-12-04 14:46:18 +0100

    Update cheetcheat.js

--- cheetcheat.js
+++ cheetcheat.js
@@ -13,8 +13,13 @@ Logische Operatoren: ==, !=, <, >, >=, <=, ===, !==
 
 let x =[2,3,"Hello"]
 console.log(x[0]) = 2
-x.push(3) = [2,3,"Hello",3]
+x.push(3) = [2,3,"Hello",3] // fügt einen wert ans ende der Liste
 x.pop() = "Hello",x = [2,3]
 x.shift() = 2,x = [3,"Hello"]
-x.slice(1,3) = [3,"Hello"]
+x.slice(1,3) = [3,"Hello"] // mit slice kann man auch sachen ersetzen
 x.includes(2) = true
+
+let n = [1,2,3,4]
+console.log(n.length)
+n.splice(3,0"Hallo")
+console.log(n)
