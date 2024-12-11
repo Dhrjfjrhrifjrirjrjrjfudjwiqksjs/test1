75b9ec6a1df8a5ff07b3065195b5c41d54d43dcb
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-12-11 15:43:32 +0100

    Create Schleifen11.12.2024.js

---
+++ Schleifen11.12.2024.js
@@ -0,0 +1,7 @@
+for(quadratzahlen = 0; quadratzahlen <=10;quadratzahlen++) {
+    console.log(quadratzahlen * quadratzahlen+"");}
+
+
+geradeZahlen = 0
+while(geradeZahlen < 20,){
+    console.log(geradeZahlen)}
