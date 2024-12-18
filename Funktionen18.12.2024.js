c0fcaff047b0c133d6ddc7f5a566b0ecd12307d9
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2024-12-18 15:42:54 +0100

    Create Funktionen18.12.2024.js

---
+++ Funktionen18.12.2024.js
@@ -0,0 +1,31 @@
+function zahlen(a,b){
+    for (let i =  a;i < b; i++){
+
+        if (i % 2 == 1 ){
+            console.log(i)
+        }
+    }
+}
+
+
+
+zahlen(1,10)
+
+
+
+function namen(name){
+console.log("Hello "+ name)}
+
+
+
+console.log(namen("Paul"))
+
+
+
+function vieleSätze(Solo_Leveling_ist_der_beste_Manga_aller_zeiten){
+    
+}
+
+
+
+        
