e4967c73c4bd6eec4e8757303226ebeae21f6dc1
Dhrjfjrhrifjrirjrjrjfudjwiqksjs <144801676+Dhrjfjrhrifjrirjrjrjfudjwiqksjs@users.noreply.github.com>
2025-02-26 14:43:25 +0100

    Create first.js

---
+++ first.js
@@ -0,0 +1,39 @@
+
+//Aufgabe 1
+
+let a = 5
+let b = 10
+
+console.log(a+b)
+
+//Aufgabe 2
+
+let x = "Paul"
+
+
+console.log("hallo"+(x))
+
+
+//Aufgabe 3
+
+let Zahlenausgabe = [1,2,3,4,5]
+console.log(Zahlenausgabe)
+
+//Aufgabe 4
+
+let viele_zahlen = [2,4,6,8,10]
+summe = 0
+for (element of viele_zahlen){
+summe = element + summe 
+}
+
+
+
+console.log(summe)
+
+
+//Aufgabe 5 
+
+
+
+
